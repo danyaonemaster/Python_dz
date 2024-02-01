f9ca61f7f0b1d64ccebc60c02bf5ade622aaf3b1
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-02-01 21:21:43 +0200

    Update main..py

--- main..py
+++ main..py
@@ -3,10 +3,12 @@ import task_02
 import task_03
 import task_04
 
-tasks = [task_01.run,
+tasks = [
+         task_01.run,
          task_02.run,
          task_03.run,
-         task_04.run]
+         task_04.run
+]
 
 for f in tasks:
     f()
