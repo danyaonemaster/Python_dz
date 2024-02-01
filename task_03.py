f206f4c7d4615261e99aaf1859aa518930383984
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-02-01 21:26:07 +0200

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -12,18 +12,17 @@ def run():
     text_without_spaces_02 = "".join(text.split(" "))
 
     text_a_list = [i for i in text.lower() if i == "a"]
-    enter = "\n"
 
-    print(f"\n\n\nlen proposals list : {len(text_list[:-1])}{enter}"
-          f"len spase list : {len(text_space_list)}{enter}"
-          f"len a list : {len(text_a_list)}{enter}"
-          f"lower case list : {text.lower()}{enter}"
-          f"text without spaces 01 : {text_without_spaces_01}{enter}"
-          f"text without spaces 02 : {text_without_spaces_02}{enter}"
+    print(f"\n\n\nlen proposals list : {len(text_list[:-1])}",
+          f"len spase list : {len(text_space_list)}",
+          f"len a list : {len(text_a_list)}",
+          f"lower case list : {text.lower()}",
+          f"text without spaces 01 : {text_without_spaces_01}",
+          f"text without spaces 02 : {text_without_spaces_02}",
           f"replacing words in text : {text.replace("Python", "Java")
-          .replace("ChatGPT", "Python")}{enter}"
-          f"every fifth character in text : {text[::5]}{enter}")
+          .replace("ChatGPT", "Python")}",
+          f"every fifth character in text : {text[::5]}", sep='\n\n')
 
-    print(f"len text is {"even" if len(text) % 2 == 0 else "odd"}")
+    print(f"len text is {"even" if len(text) % 2 == 0 else "odd"}", end="\n\n")
 
-    print(f"text in reverse order : {text[::-1]}{enter}")
+    print(f"text in reverse order : {text[::-1]}",end="\n\n")
